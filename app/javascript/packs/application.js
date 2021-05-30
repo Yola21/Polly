d9fb251566b535cb7b4105c74e17a9e8f5229f8a
Yola21 <yashkhorja4@gmail.com>
2021-05-30 13:16:33 +0530

    changed application.js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 require("@rails/ujs").start(); 
 require("@rails/activestorage").start(); 
 require("channels"); 
-require("stylesheets/application.scss"); 
+require("stylesheets/application.scss");
 var componentRequireContext = require.context("src", true); 
 var ReactRailsUJS = require("react_ujs"); 
 ReactRailsUJS.useContext(componentRequireContext); // Support component names relative to this directory:
